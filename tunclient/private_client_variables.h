1360523437a35a686171eef6e0220e196e181194
Nikolai <kolya.sviridoff@yandex.ru>
2017-07-29 08:18:34 +0300

    define N_THREADS

--- tunclient/private_client_variables.h
+++ tunclient/private_client_variables.h
@@ -8,15 +8,27 @@
 #define T_A 1
 #define T_TXT 16
 
+#define N_THREADS 5
+
 
 typedef struct Client_resource
 {
   int sock;
+  struct sockaddr_in server;
+  int *mas_sock;
   char *input_file;
   char *output_file;
   FILE *input_file_descr;
   FILE *output_file_descr;
 }Client_resource;
+
+typedef struct Thread_resource
+{
+  int sock;
+  struct sockaddr_in server;
+  FILE *input_file_descr;
+  FILE *output_file_descr;
+}Thread_resource;
 
 
 #endif /*#ifndef PRIVATE_VARIABLES_H_*/
